773fd8c2078b776be7659b2d4b0d450ace82e82b
kiua0099-sketch <kiua0099@gmail.com>
2026-06-24 21:19:35 +0300

    LauncherMenuButton.java

--- app_pojavlauncher/src/main/java/com/kdt/mcgui/LauncherMenuButton.java
+++ app_pojavlauncher/src/main/java/com/kdt/mcgui/LauncherMenuButton.java
@@ -31,20 +31,23 @@ public class LauncherMenuButton extends ExtendedButton {
 
 
     /** Set style stuff */
-    private void setSettings(){
+        private void setSettings(){
         Resources resources = getContext().getResources();
-
+        
         int padding = resources.getDimensionPixelSize(R.dimen._22sdp);
         setCompoundDrawablePadding(padding);
         setPaddingRelative(padding, 0, 0, 0);
         setGravity(Gravity.CENTER_VERTICAL);
 
-        setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimensionPixelSize(R.dimen._12ssp));
+        // Zyron Smooth Corners Edge Logic
+        setBackgroundResource(R.drawable.zyron_button);
 
-        // Set drawable size
+        setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimensionPixelSize(R.dimen._12ssp));
+        
         int[] sizes = getExtendedViewData().getSizeCompounds();
         sizes[0] = resources.getDimensionPixelSize(R.dimen._30sdp);
         getExtendedViewData().setSizeCompounds(sizes);
         postProcessDrawables();
+        }
     }
 }
